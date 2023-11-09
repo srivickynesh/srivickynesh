404ba1aae9296bb9c7d2b89a6082786da2ae2a2d
Sri Vignesh <sselvan@redhat.com>
2023-11-09 13:06:47 +0530

    Update update_readme.py

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -3,29 +3,34 @@ import re
 import requests
 from github import Github
 
+def get_github_token():
+    return os.getenv('GITHUB_TOKEN')
+
+QUOTE_API_URL = "https://quotes.rest/qod?category=inspire"
+
 def get_daily_quote():
-    response = requests.get("https://quotes.rest/qod?category=inspire")
-    if response.status_code == 200:
+    response = requests.get(QUOTE_API_URL)
+    if response.ok:  # More idiomatic way to check for status code 200
         json_response = response.json()
         quote = json_response['contents']['quotes'][0]['quote']
         author = json_response['contents']['quotes'][0]['author']
         return quote, author
     else:
-        raise Exception("Failed to fetch quote")
+        response.raise_for_status()
 
 def update_readme(quote, author):
-    with open("README.md", "r") as file:
+    with open("README.md", "r", encoding='utf-8') as file:  # Specify encoding
         content = file.read()
 
     new_content = re.sub(
-        r'(?<=<\/p>)(.*)(?=<\/p>)',
-        f'\n\n<center><p style="font-size: 1.5em; font-weight: bold; color: #1e90ff;">{quote}</p><p>- {author}</p></center>',
+        r'(?<=<!-- START QUOTE -->)(.*)(?=<!-- END QUOTE -->)',
+        f'\n\n<center><p>{quote}</p><p>- {author}</p></center>',
         content,
         flags=re.DOTALL
     )
 
     if content != new_content:
-        with open("README.md", "w") as file:
+        with open("README.md", "w", encoding='utf-8') as file:  # Specify encoding
             file.write(new_content)
         return True
     return False
@@ -33,6 +38,15 @@ def update_readme(quote, author):
 if __name__ == "__main__":
     quote, author = get_daily_quote()
     if update_readme(quote, author):
-        token = ${{ secrets.GITHUB_TOKEN }}
+        token = get_github_token()
+        if not token:
+            raise Exception("GitHub token not found")
         repo = Github(token).get_repo("srivickynesh/srivickynesh")
-        repo.update_file("README.md", "Update daily quote", open("README.md", "r").read(), repo.get_contents("README.md").sha)
+        readme_content = open("README.md", "r", encoding='utf-8').read()
+        repo.update_file(
+            "README.md",
+            "Update daily quote",
+            readme_content,
+            repo.get_contents("README.md").sha,
+            branch="main"  # Specify the branch if needed
+        )
